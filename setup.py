d3286bcf9addde2ceaa233dfdaf625691fb024d8
Christopher Johnstone <meson800@gmail.com>
2020-02-03 20:23:51 -0500

    Added minimal setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md" ,"r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="tame",
+    version="0.0.1",
+    url='https://github.com/meson800/tame',
+    author="Christopher Johnstone"
+    author_email="meson800@gmail.com",
+    description="A minimal YAML-based metadata management system.",
+    license='MIT',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=["tame"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        ],
+    install_requires=['PyYAML'],
+    python_requires='>=3'
+)
