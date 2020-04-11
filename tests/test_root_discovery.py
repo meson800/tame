83d6332f7ecee4dd99b19e4fb9e4f14370840f7f
Christopher Johnstone <meson800@gmail.com>
2020-04-11 19:06:25 -0400

    Wrapped pytest tmpdir in strpath, to satisfy python 3.5

--- tests/test_root_discovery.py
+++ tests/test_root_discovery.py
@@ -10,9 +10,9 @@ def touch(filename):
 
 def test_simple_find_root(tmpdir):
     """Tests if a simple flat hierarchy tame.yaml file is found"""
-    touch(os.path.join(tmpdir, 'tame.yaml'))
+    touch(os.path.join(tmpdir.strpath, 'tame.yaml'))
     # Does not throw exception on success
-    print(tame.core.find_root_yaml(tmpdir))
+    print(tame.core.find_root_yaml(tmpdir.strpath))
 
 def test_root_not_found(tmpdir):
     """
@@ -26,11 +26,11 @@ def test_find_root_recursive(tmpdir):
     """
     Makes sure that the root file can be found by searching upward
     """
-    os.makedirs(os.path.join(tmpdir, 'nested', 'deeply'))
-    touch(os.path.join(tmpdir, 'tame.yaml'))
-    touch(os.path.join(tmpdir, 'nested', 'metadata.yaml'))
-    touch(os.path.join(tmpdir, 'nested', 'deeply', 'test.yaml'))
-    touch(os.path.join(tmpdir, 'nested', 'deeply', 'test.jpg'))
-    print(tame.core.find_root_yaml(os.path.join(tmpdir, 'nested')))
-    print(tame.core.find_root_yaml(os.path.join(tmpdir, 'nested', 'deeply')))
-    print(tame.core.find_root_yaml(os.path.join(tmpdir, 'nested', 'metadata.yaml')))
+    os.makedirs(os.path.join(tmpdir.strpath, 'nested', 'deeply'))
+    touch(os.path.join(tmpdir.strpath, 'tame.yaml'))
+    touch(os.path.join(tmpdir.strpath, 'nested', 'metadata.yaml'))
+    touch(os.path.join(tmpdir.strpath, 'nested', 'deeply', 'test.yaml'))
+    touch(os.path.join(tmpdir.strpath, 'nested', 'deeply', 'test.jpg'))
+    print(tame.core.find_root_yaml(os.path.join(tmpdir.strpath, 'nested')))
+    print(tame.core.find_root_yaml(os.path.join(tmpdir.strpath, 'nested', 'deeply')))
+    print(tame.core.find_root_yaml(os.path.join(tmpdir.strpath, 'nested', 'metadata.yaml')))
