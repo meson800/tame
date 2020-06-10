c87341c74fa24c34adc50d7bed695d73124b5877
Christopher Johnstone <meson800@gmail.com>
2020-06-10 14:05:58 -0400

    Fixed PyQt5 version to 5.14.0

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setuptools.setup(
         'PyYAML',
         'scandir;python_version<="3.4"',
         'colorama',
-        'PyQt5',
+        'PyQt5==5.14.0',
         ],
     python_requires='>=3'
 )
